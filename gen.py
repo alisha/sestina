1fbdad3fa78b4374927bfde1aed7cd4f1693c665
Alisha Ukani <alishaaukani@gmail.com>
2017-11-15 22:34:24 -0500

    Get six words from corpus

---
+++ gen.py
@@ -0,0 +1,19 @@
+import json, random
+from pprint import pprint
+
+# Build corpus
+# Datasets in "data" are from corpora
+# https://github.com/dariusk/corpora
+corpus = json.load(open('data/objects/objects.json'))['objects']
+oneWordCorpus = [x for x in corpus if x.count(' ') == 0]
+
+# Grab six words from corpus
+# Right now these will just be nouns
+# TODO: add support for other types of words
+repeated_words = []
+for x in xrange(0,6):
+  index = random.randrange(len(oneWordCorpus))
+  repeated_words.append(oneWordCorpus[index])
+
+pprint(repeated_words)
+# Generate phrases that contain sets of words
