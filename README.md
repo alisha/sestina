e8a25e930b90216193fd806d26515afc74b6a724
Alisha Ukani <alishaaukani@gmail.com>
2017-11-17 17:34:43 -0500

    Print 50,000 words

---
+++ README.md
@@ -0,0 +1,51 @@
+# Sestina Generator
+
+**For [National Novel Generation Month 2017](https://github.com/NaNoGenMo/2017)**
+
+A [sestina poem](https://en.wikipedia.org/wiki/Sestina) is a poem that features six stanzas (five 6-line stanzas and a 3-line envoi), and six words that are repeated in specific places. This program uses [corpora datasets](https://github.com/dariusk/corpora) and [tracery](tracery.io) to generate these poems.
+
+## Absurdist Unity poems
+
+The `absurdist-unity.py` file generates sestina poems that shows the unity among various people through the objects they use.
+
+Here's one example:
+
+    The filling machine operator poured a sandglass
+    The dry-cleaning worker sniffed a fridge
+    The social service assistant stripped a sofa
+    The expediting clerk sprouted a vase
+    The podiatrist prayed a water
+    The hoist and winch operator described a sandglass
+    The claims examiner visited a tomato
+
+    The faller excused a tomato
+    The software engineer surprised a fridge
+    The psychiatrist admitted a sandglass
+    The hotel desk clerk reigned a sofa
+    The vocational counselor communicated a water
+    The door-to-door sales worker fancied a vase
+
+    The mobile home installer cured a vase
+    The paving equipment operator tested a tomato
+    The training and development specialist muddled a water
+    The hostler sprayed a fridge
+    The apparel patternmaker relaxed a sofa
+    The shaper pleased a sandglass
+
+    The blockmason hopped a sandglass
+    The auto damage insurance appraiser suspended a vase
+    The editor banned a sofa
+    The veterinary assistant stained a tomato
+    The gynecologist married a fridge
+    The hand packer sparked a water
+
+    The automotive and watercraft service attendant messed up a water
+    The radiologic technologist returned a sandglass
+    The adult education teacher observed a fridge
+    The power plant operator pulled a vase
+    The cargo agent prepared a tomato
+    The electrical installer pecked a sofa
+
+    The drier operator sailed a sofa and a water
+    The audiologist rinsed a tomato and a sandglass
+    The aircraft mechanic rhyme a vase and a fridge
---
+++ absurdist-unity.py
@@ -0,0 +1,95 @@
+import json, random, requests, tracery
+import secret
+from pprint import pprint
+from tracery.modifiers import base_english
+
+# DATA COLLECTION
+
+# Build corpus
+# Datasets in "data" are from corpora
+# https://github.com/dariusk/corpora
+corpus = json.load(open('data/objects/objects.json'))['objects']
+one_word_corpus = [x for x in corpus if x.count(' ') == 0]
+
+# Grab six words from corpus, will be at the end of each line
+# Right now these will just be nouns
+# TODO: add support for other types of words
+repeated_words = []
+for x in xrange(0,6):
+  index = random.randrange(len(one_word_corpus))
+  repeated_words.append(one_word_corpus[index])
+  del one_word_corpus[index]
+
+# Get occupations
+occupations = json.load(open('data/humans/occupations.json'))['occupations']
+
+# Get verbs
+all_verbs = json.load(open('data/words/verbs.json'))['verbs']
+verbs = []
+for verb in all_verbs:
+  verbs.append(verb['past'])
+
+# POEM GENERATION
+
+# Sestina poem structure
+poem_structure = [
+  [1,2,3,4,5,6],
+  [6,1,5,2,4,3],
+  [3,6,4,1,2,5],
+  [5,3,2,6,1,4],
+  [4,5,1,3,6,2],
+  [2,4,6,5,3,1]
+]
+
+# Tracery rules
+rules = {
+  'origin': 'The #occupation# #verb# #object.a#',
+  'occupation': occupations,
+  'verb': verbs,
+  'object': repeated_words
+}
+
+# Build a poem
+# Print to absurdist-unity.txt
+f = open('absurdist-unity', 'w')
+
+total_words = 0
+while total_words < 50000:
+  # Stanzas 1-5
+  for stanza in xrange(0,6):
+    line_num = 0
+    line = ""
+    while line_num < 6:
+      if stanza != 5:
+        # This word must occur at the end of the line
+        special_word = repeated_words[poem_structure[stanza][line_num] - 1]
+
+        # Generate Tracery rules
+        rules['object'] = special_word
+        grammar = tracery.Grammar(rules)
+        grammar.add_modifiers(base_english)
+        line = grammar.flatten("#origin#")
+
+      # Last stanza has a special structure
+      else:
+        # Generate Tracery rules
+        rules['origin'] = 'The #occupation# #verb# #object1.a# and #object2.a#'
+        rules['object1'] = repeated_words[poem_structure[stanza][line_num] - 1]
+        rules['object2'] = repeated_words[poem_structure[stanza][line_num + 1] - 1]
+
+        rules['object'] = special_word
+        grammar = tracery.Grammar(rules)
+        grammar.add_modifiers(base_english)
+        line = grammar.flatten("#origin#")
+
+        line_num += 1
+
+      f.write(line + "\n")
+      total_words += len(line.split())
+      line_num += 1
+
+    f.write("\n")
+
+f.write("\nTOTAL WORDS: " + str(total_words))
+f.close()
+
